c42b3d2538f1701d8561a4c7bb5d1fe15ed3f032
abydhalech <94381726+abydhalech@users.noreply.github.com>
2026-09-07 18:45:13 +0100

    Remove coach hire and clean service SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
   siteUrl: 'https://oakdaletravel.co.uk',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
-  exclude: ['/404']
+  exclude: [
+    '/404',
+    '/services/coach-hire-33-seater-small-and-medium-groups',
+  ],
 };
